f8f65ab2c3a22030cd7b4fa818d067a539724c0c
Samer Kanjo <samer@kanjo.net>
2018-05-08 20:41:43 -0500

    ref #2 Basic CLI, only version and help working.

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject hips-clj "0.1.0-SNAPSHOT"
   :description "Merge and sort records"
   :url "https://github.com/skanjo/hips-clj"
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.cli "0.3.7"]
+                 [trptcolin/versioneer "0.2.0"]]
   :main ^:skip-aot hips-clj.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :plugins [])
--- src/hips_clj/core.clj
+++ src/hips_clj/core.clj
@@ -1,7 +1,56 @@
 (ns hips-clj.core
+  (:require
+    [clojure.string :as string]
+    [clojure.tools.cli :refer [parse-opts]]
+    [trptcolin.versioneer.core :as version])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def ^{:added "0.1.0"} cli-spec
+  [["-v" "--version" "Version of this application"]
+   ["-h" "--help" "Prints this help message"]])
+
+(defn- ^{:added "0.1.0"} cli-help-msg [summary]
+  (->> ["HipsCli merges and sorts one or more files containing person records for profit!"
+        ""
+        "Usage: HipsCli [options] [file ...]"
+        ""
+        "Options:"
+        summary
+        ""]
+       (string/join \newline)))
+
+(defn- ^{:added "0.1.0"} cli-version-msg []
+  (str "HipsCli" " " (version/get-version "hips-clj" "hips-clj"))
+  )
+
+(defn- ^{:added "0.1.0"} cli-error-msg [errors]
+  (->> ["The following errors occurred while parsing your command:"
+        ""
+        (string/join \newline errors)]
+       (string/join \newline)))
+
+(defn- ^{:added "0.1.0"} cli-parse-command [args]
+  (let [{:keys [options arguments summary errors]} (parse-opts args cli-spec)]
+    (cond
+      (:help options)
+      {:command :help :message (cli-help-msg summary)}
+
+      (:version options)
+      {:command :version :message (cli-version-msg)}
+
+      errors
+      {:command :errors :message (cli-error-msg errors)}
+
+      :else
+      {:command :help :message (cli-help-msg summary)}
+      )))
+
+(defn ^{:added "0.1.0"} -main [& args]
+  (let [{:keys [command message]} (cli-parse-command args)]
+    (case command
+      :help (println message)
+      :version (println message)
+      :exit message (println message)
+      )
+    ))
+
